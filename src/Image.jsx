485d3e1fb0927e0dea2639faca65a8a698f9406d
ziadmohamed30 <ziadmohamed9816@gmail.com>
2025-07-18 02:50:02 +0300

    s

--- src/Image.jsx
+++
@@ -1,7 +0,0 @@
-export default function Image1() {
-  return (
-    <div>
-      <img className="object-fit-cover col-12" src={Image1} alt="" />
-    </div>
-  );
-}
--- src/LoremOne.jsx
+++ src/LoremOne.jsx
@@ -1,11 +1,7 @@
-import Image1 from "src/1.png";
-
 export default function LoremOne() {
   return (
     <div className="one d-flex flex-column col-3 gap-4">
-      <div className="circle bg-white  ">
-        <Image1/>
-      </div>
+      <div className="circle bg-white  "></div>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
